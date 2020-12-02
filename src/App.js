a9370f89b75f63768bc27ed5b1df9327bf13b4e3
Nikhil Rai <nikhilrai637@gmail.com>
2020-12-02 20:41:13 +0530

    adding checkbox and using input props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,59 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
+import CheckBox from '@material-ui/core/CheckBox';
+
+
+function CheckBoxExample () {
+      const  [checked , setChecked ] = React.useState(true)
+  return(
+    <div>
+      <CheckBox
+      color="primary"
+      checked = {checked}
+      onChange={(event) =>setChecked(event.target.checked)}
+      inputProps = {{'arie-label' : 'secondary checkbox'}}
+
+      /> 
+    </div>
+  )
+
+}
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ButtonGroup          variant = "contained"  color="primary">
-        <Button onClick={()=> alert("got Clicked")}
-         
-          startIcon = {<SaveIcon/>}
+        <CheckBoxExample/>
+        <ButtonGroup variant="contained" color="primary">
+          <Button onClick={() => alert("got Clicked")}
+
+            startIcon={<SaveIcon />}
 
 
           >
-           Click Me!
+            Click Me!
            </Button>
 
-           <Button onClick={()=> alert("got Clicked")}
-         
-         startIcon = {<DeleteIcon/>}
+          <Button onClick={() => alert("got Clicked")}
 
-       
-       
-         >
-          Ignore Me!
+            startIcon={<DeleteIcon />}
+
+
+
+          >
+            Ignore Me!
           </Button>
         </ButtonGroup>
 
         <img src={logo} className="App-logo" alt="logo" />
-      
+
         <a
           className="App-link"
           href="https://reactjs.org"
