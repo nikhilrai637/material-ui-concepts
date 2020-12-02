a84c7db615e2a07e93ab038337c1ecca1ce3204e
Nikhil Rai <nikhilrai637@gmail.com>
2020-12-02 20:25:07 +0530

    adding and implementing button group

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ButtonGroup          variant = "contained"  color="primary">
         <Button onClick={()=> alert("got Clicked")}
          
           startIcon = {<SaveIcon/>}
 
-         variant = "contained" 
-         color="secondary"
+
           >
            Click Me!
            </Button>
+
+           <Button onClick={()=> alert("got Clicked")}
+         
+         startIcon = {<DeleteIcon/>}
+
+       
+       
+         >
+          Ignore Me!
+          </Button>
+        </ButtonGroup>
+
         <img src={logo} className="App-logo" alt="logo" />
       
         <a
