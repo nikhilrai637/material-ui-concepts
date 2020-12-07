abf5792bd018cd643d957a4aab65ab350f869dc3
Nikhil Rai <nikhilrai637@gmail.com>
2020-12-07 23:20:04 +0530

    Adding Card Media , Multiple cards from stationary db

--- src/CoffeeCard.js
+++ src/CoffeeCard.js
@@ -9,6 +9,7 @@ import Avatar from '@material-ui/core/Avatar';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const useStyles = makeStyles({
    
@@ -25,44 +26,39 @@ const useStyles = makeStyles({
   },
 });
 
-export default function CoffeeCard() {
+export default function CoffeeCard(props) {
   const classes = useStyles();
+  const {avatarUrl , title , subtitle,description , imageUrl}  = props;
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.root}>
-              <CardHeader
+        <CardHeader
         avatar={
-          <Avatar aria-label="recipe" className={classes.avatar}>
-            R
-          </Avatar>
+          <Avatar  src = {avatarUrl} />
         }
         action={
           <IconButton aria-label="settings">
             <ShareIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={title}
+        subheader={subtitle}
+      />
+       <CardMedia
+        style = {{height:"150px"}}
+        image={imageUrl}
+        title="Paella dish"
       />
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+          {description}
         </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small">BUY NOW</Button>
+        <Button size="small">OFFER</Button>
       </CardActions>
     </Card>
   );
--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,26 @@
 import React from 'react'
 import CoffeeCard from './CoffeeCard'
 import {Grid} from '@material-ui/core'
+import CoffeList from './Constants'
+
 
 
 const Content = ()  => {
+
+    const getCoffeeCard = (coffeeObj) => {
+        return (
+            <Grid item xs = {12} sm = {6} md = {4}>
+                <CoffeeCard {...coffeeObj}></CoffeeCard>
+            </Grid>
+        )
+    }
+
     return (<div>
          <Grid container spacing = {4}>
            
-            <Grid item xs = {12} sm = {6} md = {4}  >
-             <CoffeeCard/>
-            </Grid>
-
-            <Grid item xs = {12} sm = {6} md = {4} >
-             <CoffeeCard/>
-            </Grid>
-
-            <Grid item xs = {12} sm = {6} md = {4} >
-             <CoffeeCard/>
-            </Grid>
-
-            <Grid item xs = {12} sm = {6} md = {4} >
-             <CoffeeCard/>
-            </Grid>
-
-            <Grid item xs = {12} sm = {6} md = {4}  >
-             <CoffeeCard/>
-            </Grid>
+         {CoffeList.map(coffeeObj => {
+            return getCoffeeCard(coffeeObj);
+         })}    
          
          </Grid>
 
