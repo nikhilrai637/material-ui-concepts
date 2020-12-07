c98b14472bddafc7e36cbb03c84243332770df1c
Nikhil Rai <nikhilrai637@gmail.com>
2020-12-07 20:28:18 +0530

    Share icon

--- src/Content.js
+++ src/Content.js
@@ -5,16 +5,25 @@ import {Grid} from '@material-ui/core'
 
 const Content = ()  => {
     return (<div>
-         <Grid container spacing = {2}>
-            <Grid item xs = {4}  >
+         <Grid container spacing = {4}>
+           
+            <Grid item xs = {12} sm = {6} md = {4}  >
              <CoffeeCard/>
             </Grid>
 
-            <Grid item xs = {4}   >
+            <Grid item xs = {12} sm = {6} md = {4} >
              <CoffeeCard/>
             </Grid>
 
-            <Grid item xs = {4}  >
+            <Grid item xs = {12} sm = {6} md = {4} >
+             <CoffeeCard/>
+            </Grid>
+
+            <Grid item xs = {12} sm = {6} md = {4} >
+             <CoffeeCard/>
+            </Grid>
+
+            <Grid item xs = {12} sm = {6} md = {4}  >
              <CoffeeCard/>
             </Grid>
          
