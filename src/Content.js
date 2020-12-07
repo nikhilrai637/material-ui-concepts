ebf021e565f4fe05834871c97f6ddcec495260f0
Nikhil Rai <nikhilrai637@gmail.com>
2020-12-07 20:20:40 +0530

    adding padding

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import CoffeeCard from './CoffeeCard'
+import {Grid} from '@material-ui/core'
 
 
 const Content = ()  => {
-    return (<h1>This is our header</h1>)
+    return (<div>
+         <Grid container spacing = {2}>
+            <Grid item xs = {4}  >
+             <CoffeeCard/>
+            </Grid>
+
+            <Grid item xs = {4}   >
+             <CoffeeCard/>
+            </Grid>
+
+            <Grid item xs = {4}  >
+             <CoffeeCard/>
+            </Grid>
+         
+         </Grid>
+
+    </div>
+    )
 }
 
 export default Content
