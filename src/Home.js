9e0225ede8a13e3fd9b304af17a69becbc2d23f9
Nikhil Rai <nikhilrai637@gmail.com>
2020-12-31 22:05:33 +0530

    Toggling between about and home

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
 import React , {useState} from 'react'
 import {Tabs , Tab , AppBar} from "@material-ui/core"
+import Contact from './Contact'
+import About from './About'
 function Home() {
     const [selectedTab, setselectedTab] = useState(0)
     const handleChange = (event , newValue) => setselectedTab(newValue)
     return (
+    <>
+    <AppBar position = "static">
     <Tabs value={selectedTab} onChange={handleChange} aria-label="simple tabs example">
         <Tab label="About"/>
         <Tab label="Contact us" />
-         
     </Tabs>
+    </AppBar>
+    {selectedTab === 0 && <About/>}
+    {selectedTab === 1 && <Contact/>}
+    </>
     )
 }
 
